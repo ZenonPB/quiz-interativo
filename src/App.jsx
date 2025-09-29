ebfe0cef99f52b2c799b917bbfe30a81357bce13
ZenonPB <zenon.bergamo321@gmail.com>
2025-09-29 10:02:21 -0300

    ADD: QuestionCard implementado

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,33 @@ import { useState } from 'react'
 import styles from './App.module.css'
 import Header from './components/Header'
 import QuestionCard from './components/QuestionCard'
+import questions from './data/questions.js'
 // import ScoreBoard from './components/ScoreBoard'
 
 
 function App() {
   const [current, setCurrent] = useState(0);
-  const [step, setStep] = useState("quiz");
+  const [answer, setAnswer] = useState([]); // armazena as respostas do usuario
+  const [step, setStep] = useState("quiz"); // define a parte do quiz que o usuario está (quiz ou resultado)
+
+  const handleAnswer = (option, timeSpent) => {
+    setAnswer(anterior => [
+      ...anterior,
+      {
+        questionId: questions[current].id,
+        answer: option,
+        time: timeSpent
+      }
+    ]);
+
+    // avança para a próxima questão ou para o resultado, dependendo do nº da questão atual
+    if (current < questions.length - 1) {
+      setCurrent(current + 1);
+    } else {
+      setStep("resultado");
+    }
+
+  };
 
 
   return (
@@ -19,11 +40,13 @@ function App() {
             <Header 
               title="Quiz Interativo"
               current={current + 1}
-              // total={questions.length}
+              total={questions.length}
             />
 
             <QuestionCard 
-            
+              key={current}
+              data={questions[current]}
+              onAnswer={handleAnswer}
             />
           </>
         ) : (
--- src/components/QuestionCard/index.jsx
+++ src/components/QuestionCard/index.jsx
@@ -1,3 +1,35 @@
-export default function QuestionCard() {
-    
+import { useEffect } from "react";
+import { useState } from "react";
+import styles from './QuestionCard.module.css'
+
+export default function QuestionCard({ data, onAnswer }) {
+    const [time, setTime] = useState(0);
+
+    useEffect(() => {
+        setTime(0);
+        const timer = setInterval(() => setTime((t) => t + 1), 1000);
+        return () => clearInterval(timer);
+    }, [data]); 
+
+    const handleClick = (option) => {
+        onAnswer(option, time); // pega apenas as infos necessarias para o feedback (se acertou e o tempo)
+    }
+
+    return(
+        <section className={styles.card}>
+            <h2>{data.question}</h2>
+            <span>{data.category}</span>
+
+            <div className={styles.options}>
+                {data.options.map(option => (
+                    <button onClick={() => handleClick(option)} key={option}>
+                        {option}
+                    </button>
+                ))}
+            </div>
+
+            
+        </section>
+    )
+
 }
